6f3f583983f7fc5b612f6a2292418da94e041872
urolyi1 <urolyi1@gmail.com>
2019-10-02 22:58:04 -0400

    Delete extraneous testing code

--- neural_net.py
+++ neural_net.py
@@ -40,7 +40,7 @@ class NeuralNet:
 
         # for each layer
         for idx, layer in enumerate(self.weights):
-            # layer's output = transition_func(prev_input * weights)
+            # layer's output = transition_func(prev_input * weights) + bias
             activation_val = np.add(np.dot(prev_input, layer), self.bias[idx])
             self.activations.append(activation_val)
             prev_input = self.trans_func(self.activations[idx])
@@ -49,14 +49,18 @@ class NeuralNet:
 
     def backprop(self, y, num_samples):
         ''' Optimize weights through backpropagation '''
-        # iterate over 
+
+        # iterate over outputs and activation input values
         iterable = enumerate(zip(reversed(self.outputs), reversed(self.activations)))
         for idx, (output_val, activation_val) in iterable:
             layer_idx = self.num_layers - idx - 1
+
+            # if last layer dz = y_hat - y else previous dz * weights * derivative of transition function
             if layer_idx == self.num_layers - 1:
                 dz = output_val - y
             else:
                 dz = np.multiply(np.dot(dz, self.weights[layer_idx].T), d_sigmoid(self.activations[layer_idx - 1]))
+
             # Changing weights in direction of gradient
             dw = np.dot(self.outputs[layer_idx - 1].T, dz) / num_samples
             self.weights[layer_idx - 1] -= dw * self.learn_rate
@@ -75,35 +79,3 @@ class NeuralNet:
 
         optimized_out = self.feedforward(test_x)
         print(self.get_loss(test_y, optimized_out))
-
-
-test = NeuralNet([2, 20, 20, 2])
-train_x = np.random.rand(10000, 2)
-train_y = np.concatenate(([np.sin(train_x[:,0] * train_x[:,1])], [np.exp(train_x[:,0] - train_x[:,1])])).T
-#np.concatenate(([(train_x[:,0] + train_x[:,1])], [(train_x[:,0] - train_x[:,1])], 
-#                           np.abs([(train_x[:,0] - train_x[:,1])]))).T
-
-test_x = np.random.rand(1000, 2)
-test_y = np.concatenate(([np.sin(test_x[:,0] * test_x[:,1])], [np.exp(test_x[:,0] - test_x[:,1])])).T
-#np.concatenate(([(test_x[:,0] + test_x[:,1])], [(test_x[:,0] - test_x[:,1])],
-#                          np.abs([(test_x[:,0] - test_x[:,1])]))).T
-
-test.train(1000, train_x, train_y, test_x, test_y)
-'''
-test = NeuralNet([2, 40, 40, 3])
-epochs = 100
-for i in range(1, epochs):
-    x = np.random.rand(1000, 2)
-    y = np.concatenate(([(x[:,0] + x[:,1])], [(x[:,0] - x[:,1])], np.abs([(x[:,0] - x[:,1])]))).T
-    y_hat = test.feedforward(x)
-    print(test.get_loss(y, y_hat))
-    test.backprop(y, 1000)
-    y_hat = test.feedforward(x)
-    print(test.get_loss(y, y_hat))
-x = np.random.rand(10000, 2)
-y = np.concatenate(([(x[:,0] + x[:,1])], [(x[:,0] - x[:,1])], np.abs([(x[:,0] - x[:,1])]))).T
-y_hat = test.feedforward(x)
-print(test.get_loss(y, y_hat))
-print(test.feedforward([[0.5, 0.5]]))
-print(test.bias)
-'''
